064c86ead087b349ac1a30cef656b8a3ac2d751f
Sabrina Bell <skibri@yahoo.com>
2017-09-29 19:31:47 -0700

    session variables working sort of

--- homepage.php
+++ homepage.php
@@ -12,16 +12,40 @@
 <?php
 //SET VARIABLES
 //ex:  $_SESSION["name"] = value
-   $_SESSION["peterPan"] = 0;
-   $_SESSION["aliceInWonderland"] = 0;
-   $_SESSION["harryPotter"] = 0;
-   $_SESSION["wizardOfOz"] = 0;
-   $_SESSION["hobbit"] = 0;
-   $_SESSION["twilight"] = 0;
-   $_SESSION["xMen101"] = 0;
-   $_SESSION["summerFalls"] = 0;
+   $_SESSION['peterPan'] = 0;
+   $_SESSION['aliceInWonderland'] = 0;
+   $_SESSION['harryPotter'] = 0;
+   $_SESSION['wizardOfOz'] = 0;
+   $_SESSION['hobbit'] = 0;
+   $_SESSION['twilight'] = 0;
+   $_SESSION['xMen101'] = 0;
+   $_SESSION['summerFalls'] = 0;
 ?>
 
+<?php   
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      if(isset($_POST["peterPan"]))
+         $_SESSION['peterPan'] = $_POST['peterPan'];
+      if(isset($_POST["aliceInWonderland"]))
+         $_SESSION['aliceInWonderland'] = $_POST['aliceInWonderland'];
+      if(isset($_POST["harryPotter"]))
+         $_SESSION['harryPotter'] =  $_POST['harryPotter'];
+      if(isset($_POST["wizardOfOz"]))
+         $_SESSION['wizardOfOz'] = $_POST['wizardOfOz'];
+      if(isset($_POST["hobbit"]))
+         $_SESSION['hobbit'] = $_POST['hobbit'];
+      if(isset($_POST["twilight"]))
+         $_SESSION['twilight'] = $_POST['twilight'];
+      if(isset($_POST["xMen101"]))
+         $_SESSION['xMen101'] = $_POST['xMen101'];
+      if(isset($_POST["summerFalls"]))
+         $_SESSION['summerFalls'] = $_POST['summerFalls'];
+   }
+?>
+
+<?php
+   print_r($_SESSION);
+?>
 
    <div id="header">
       <div id="storeName">
@@ -55,28 +79,35 @@
                <img class="card-img-top" src="https://i.pinimg.com/736x/82/f0/b0/82f0b06b11114bed643691ff5836763c--peter-pan-peter-otoole.jpg" alt="Peter Pan">
                <h4 class="card-title text-center">Peter Pan</h4>
                <p class="card-text">Written by J.M. Barrie, this classic tells the story of a boy who never grows up.</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <form action"" method="post" class="card-footer">
+                  <input type="submit" value="Add to cart" name="peterPan">
+               </form>
+
             </div>
 
             <div class="card">
                <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/91eLLDMzQ3L.jpg" alt="Alice in Wonderland">
                <h4 class="card-title text-center">Alice in Wonderland</h4>
                <p class="card-text">Alice falls down the rabbit hole and finds a world of white rabbits, tea parties, and mad queens!</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <input type="submit" class="card-footer" value="Add to cart" name="aliceInWonderland">
             </div>
 
             <div class="card">
                <img class="card-img-top" src="http://harrypotterfanzone.com/wp-content/2009/06/ss-us-jacket-art.jpg" alt="Harry Potter">
                <h4 class="card-title text-center">Harry Potter and the Sorcerer’s Stone</h4>
                  <p class="card-text">The first in the worldwide phenomenon, follow the journey of a boy who discovers that he is a wizard.</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <form action"" method="post" class="card-footer">
+                  <input type="submit" class="card-footer" value="Add to cart" name="harryPotter">
+               </form>
             </div>
 
             <div class="card">
                <img class="card-img-top" src="https://www.loc.gov/exhibits/oz/images/vc9p1.jpg" alt="Wizard of Oz">
                <h4 class="card-title text-center">The Wonderful Wizard of Oz</h4>
                <p class="card-text">This charming book is the first in a series, and inspired the classic movie. Join Dorothy over the rainbow.</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <form action"" method="post" class="card-footer">
+                  <input type="submit" class="card-footer" value="Add to cart" name="wizardOfOz">
+               </form
             </div>
          </div>  
 
@@ -85,28 +116,30 @@
                <img class="card-img-top" src="http://lisagarnerdesign.co.uk/blog/wp-content/uploads/2014/12/The_Hobbit.jpg" alt="The Hobbit">
                <h4 class="card-title text-center">The Hobbit</h4>
                <p class="card-text">Preciousss! Just kidding. This tale inspired the epic <em>Lord of the Rings</em> and follows the adventures of Bilbo Baggins.</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <input type="submit" class="card-footer" value="Add to cart" name="hobbit">
             </div>
 
             <div class="card">
                <img class="card-img-top" src="http://stepheniemeyer.com/wp-content/uploads/2016/02/twilight-book-cover.jpg" alt="Twilight">
                <h4 class="card-title text-center">Twilight</h4>
                <p class="card-text">It was a book, a movie, a mania, a meme… and now it’s for sale on our website. <em>Twilight</em>, folks.</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <input type="submit" class="card-footer" value="Add to cart" name="twilight">
             </div>
 
             <div class="card">
                <img class="card-img-top" src="http://d1466nnw0ex81e.cloudfront.net/n_iv/600/844629.jpg" alt="X-men #101">
                <h4 class="card-title text-center">X-Men #101</h4>
                <p class="card-text">The first appearance of Jean Grey’s Phoenix! And, the beginning of an amazing story arc. Plus it’s super rare.</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <input type="submit" class="card-footer" value="Add to cart" name="xMen101">
             </div>
 
             <div class="card">
                <img class="card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/51lMr46vOHL.jpg" alt="Summer Falls">
                <h4 class="card-title text-center">Summer Falls1</h4>
                <p class="card-text">Who doesn’t love a good ghostwriter? This book was (ghost)written for <em>Doctor Who</em>, and belonged to Clara Oswald, the Doctor’s companion.</p>
-               <button type="button" class="card-footer">Add to cart</button>
+               <form action"" method="post" class="card-footer">
+                  <input type="submit" class="card-footer" value="Add to cart" name="summerFalls">
+               </form>
             </div>
          </div> 
       </div>
